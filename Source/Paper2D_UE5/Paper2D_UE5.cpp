608f598cd51d65c27a0b9fef3f6d9b0755f6eb16
FunctionOverflu <ta.yu.gamedev@gmail.com>
2022-04-18 15:19:55 +0200

    Initial Commit

---
+++ Source/Paper2D_UE5/Paper2D_UE5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Paper2D_UE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Paper2D_UE5, "Paper2D_UE5" );
